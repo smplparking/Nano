e4bcbc3ccd9549b28fa1f430d1e2c11b8ec04c57
FunkeCoder23 <adt27@uakron.edu>
2021-02-17 16:19:37 -0500

    added 7seg class

--- sevenSeg.py
+++ sevenSeg.py
@@ -1,53 +1,47 @@
-import digitalio
 import board
 
 from time import sleep
 BCD = [63, 6, 91, 79, 102, 109, 125, 7, 127, 111]
 
 
-BAUDRATE = 16000000
-#set up SPI using hardware SPI
-spi=board.SPI()
-spi.try_lock()
-spi.configure(baudrate=BAUDRATE)
-spi.unlock()
-
-bigboard = False
-
-
-def getDisplayCode(num):
-    return BCD[num]
+class sevenseg:
+    def __init__(self):
+        BAUDRATE = 16000000
+        #set up SPI using hardware SPI
+        self.spi=board.SPI()
+        self.spi.try_lock()
+        self.spi.configure(baudrate=BAUDRATE)
+        self.spi.unlock()
 
 
-def us_delay(N):
-    sleep(N/1000000.0)
+    def us_delay(N):
+        sleep(N/1000000.0)
 
 
-def ms_delay(N):
-    sleep(N/1000.0)
+    def ms_delay(N):
+        sleep(N/1000.0)
 
 
-def reset():
-    spi.write([0x81])
+    def reset(self):
+        self.spi.write([0x0])
+        self.spi.write([0x0])
 
 
-def updateDisplay(count):
-    # SPI Communication w/ 7segs
+    def updateDisplay(self,count):
+        # SPI Communication w/ 7segs
 
-    if not bigboard:
-        spi.write([0x76])  # clear display
-    to_send = [count % 10, (count//10) % 10]  # LSD, MSD
-    if bigboard:
-        to_send = [getDisplayCode(x)
-                   for x in to_send]  # use table to convert to binary
-    if count > 99 and bigboard:
-        to_send = [x + 128 for x in to_send]  # if > 99, add '+'
-    spi.write(to_send)  # send spi comms
+        to_send = [count % 10, (count//10) % 10]  # LSD, MSD
+        to_send = [BCD[x]
+                    for x in to_send]  # use table to convert to binary
+        if count > 99:
+            to_send = [x + 128 for x in to_send]  # if > 99, add '+'
+        self.spi.write(to_send)  # send spi comms
+        self.spi.write(to_send)  # writing twice fixes segment issues
 
-
-reset()
+sev=sevenseg()
+sev.reset()
 
 for i in range(100):
     print(f'displaying {i}')
-    updateDisplay(i)
-    sleep(5)
+    sev.updateDisplay(i)
+    sleep(.5)
--- spitest.py
+++ spitest.py
@@ -3,6 +3,7 @@ import board
 import busio
 import digitalio
 from time import sleep
+from constants import SevenSegmentASCII
 BCD = [63, 6, 91, 79, 102, 109, 125, 7, 127, 111]
 
 
@@ -12,14 +13,25 @@ BCD = [63, 6, 91, 79, 102, 109, 125, 7, 127, 111]
 # MOSI.direction=digitalio.Direction.OUTPUT
 # CLK.direction = digitalio.Direction.OUTPUT
 
+
+def writeAscii(text):
+    out =[]
+    for char in text:
+        value = SevenSegmentASCII[ord(char)-32]
+        spi.write([value])
+        spi.write([value])
+        sleep(1)
+    return out
+
 spi=board.SPI()
-spi.try_lock()
-spi.configure(baudrate=10)
-spi.unlock()
-while True:
-    spi.write([0xff])
-    sleep(0.5)
-# spi = board.SPI()
+writeAscii("Hello World"
+)
+# while True:
+#     for digit in BCD:
+#         spi.write([digit])
+#         spi.write([digit])
+#         sleep(1)
+    # spi = board.SPI()
 # cs=digitalio.DigitalInOut(board.D6)
 # while True:
 #     for i in range(10):

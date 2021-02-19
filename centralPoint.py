8b8298185c026e17412efe3ef25f0e787dbc60f7
Joshua Newman <joshua.zips@gmail.com>
2021-02-18 23:11:21 -0500

    finished CentralPoint class

--- centralPoint.py
+++ centralPoint.py
@@ -55,11 +55,68 @@ class centralPoint:
             objectCentralPoints = list(self.objects.values())
 
             #Computing the distance between each pair of object central Points and input central Points
-            DistanceArrayOCPandICP = dist.cdist(np.array(objectCentralPoints), inputCentralPoint)
+            pointDistance = dist.cdist(np.array(objectCentralPoints), inputCentralPoint)
 
             # To match, find smallest value in each row, sort the row indexes based on their minimum values
             # so that the row with the smallest value as at the *front* of the index list
-            rows = DistanceArrayOCPandICP.min(axis=1).arsort()
+            rows = pointDistance.min(axis=1).arsort()
+
+            #Same Process but with Columns and sort with previous row index list
+            cols = pointDistance.argmin(axis=1)[rows]
+
+            #Keep track of used Rows/Columns for updating/registering/deregistering
+            usedRows = set()
+            usedCols = set()
+
+            #Put mother fuckers into tuples
+            for (row, col) in zip(rows,cols):
+                #Ignore already examined row or column
+                if row in usedRows or col in usedCols:
+                    continue
+
+                # If distance between Central Points is greater than the max distance,
+                # do not associate the two central points to the same object
+
+                if pointDistance[row,col] > self.maxDistance:
+                    continue
+
+                # Grab objectID for the current row, set its new central point, and reset
+                # the disappeared counter
+                objectID = objectIDs[row]
+                self.objects[objectID] = inputCentralPoint[col]
+                self.disappeared[objectID] = 0
+
+                #update each row and column into our used rows/cols list
+                usedRows.add(row)
+                usedCols.add(col)
+
+            # Compute row/col index we have **NOT** examined yet
+            unusedRows = set(range(0,pointDistance.shape[0])).difference(unusedRows)
+            unusedcols = set(range(0,pointDistance.shape[1])).difference(unusedcols)
+
+            # if Object Central points are >= number of input central points,
+            # we need to check and see if some of these objects have possibly disappeared
+            if pointDistance.shape[0] >= pointDistance.shape[1]:
+                for row in unusedRows:
+                    # grab objectID for row index and increment the disappeared counter
+                    objectID = objectIDs[row]
+                    self.disappeared[objectID] +=1
+
+                    #if objects disappeared frames > max disappeared, we deregister
+                    if self.disappeared[objectID] > self.maxDisappeared:
+                        self.deregister(objectID)
+
+            # if input central points > existing object central points, register
+            # each new input central point as a trackable object
+            else:
+                for col in unusedcols:
+                    self.register(inputCentralPoint[col])
+
+        #return the set of trackable objects
+        return self.objects
+
+
+
 
 
 
